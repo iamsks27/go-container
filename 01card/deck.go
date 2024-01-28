0da734d74152c2bb0c53d8de4f3a8dbb1ec64667
iamsks27 <shivamsingh1221@gmail.com>
2024-01-28 20:49:17 +0530

    Added a function to deal a deck of card

--- 01card/deck.go
+++ 01card/deck.go
@@ -10,7 +10,12 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardValues := []string{
+		"Ace",
+		"Two",
+		"Three",
+		"Four",
+	}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
@@ -25,4 +30,9 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+
+	return d[:handSize], d[handSize:]
 }
