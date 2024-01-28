4a50d8345fc842b1909d1d5b3d83266121b92b14
iamsks27 <shivamsingh1221@gmail.com>
2024-01-28 20:33:59 +0530

    Added a method to create a new deck

--- 01card/main.go
+++ 01card/main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Hearts")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Ace of Spades"
 }
