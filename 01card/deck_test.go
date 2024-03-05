a2d928f2caabc7d9996ea9ddc0b69790890c7b95
iamsks27 <shivamsingh1221@gmail.com>
2024-03-05 20:59:37 +0530

    Added a method to test writing & reading deck from a file

--- 01card/deck_test.go
+++ 01card/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,24 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected 16 cards, got %d", len(d))
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	// Removing file if already exists
+	os.Remove("_decktesting")
+
+	// Creating a new deck & saving it in a file
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	// Creating a new deck by reading the connect of a file
+	ld := newDeckFromFile("_decktesting")
+
+	// Running tests
+	if len(ld) != 16 {
+		t.Errorf("Expecting 16 cards, got %d", len(ld))
+	}
+
+	// Removing the test file
+	os.Remove("_decktesting")
 }
